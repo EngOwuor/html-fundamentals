b3e5367fe8c1697d6f3b54d2458b0c1cb3332867
robert owuor <owuorrobert76@gmail.com>
2022-03-11 16:00:10 +0300

    wired brain coffee basic pages

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+var router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'../src/index.html'));
+
+});
+router.get('/recipes.html',function(req,req){
+    req.sendFile(path.join(__dirname,"../src/recipes.html"));
+});
+router.get('/nutrition.html',function(req,res){
+    res.sendFile(path.join(__dirname,'../src/nutrition.html'));
+});
+app.use("/",router);
+
+app.listen(port,function(err){
+    if (err){
+        console.log(err)
+    }else{
+        open('http://localhost:'+ port); 
+    }
+});
